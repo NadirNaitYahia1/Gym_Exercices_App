f385daf0f0397f3b72d0287b599060ada7408ffa
Nadir <n_naityahia@estin.dz>
2023-07-16 12:16:03 +0200

    github-commit

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/hero.css';
 import '../bootstrap-5.1.3-dist/css/bootstrap.css';
 import banner from '../assets/images/banner1.jpg';
+import { useState } from 'react';
 
 const HeroBanner = () => {
+  const [width, setWidth] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth < 992) {
+      setWidth(true);
+    }
+  }, []);
+  
+
+ 
+
   return (
     <div className="container-fluid">
-      <div className='row mt-2 '>
-        <div className="hero col-lg-6 col-12 mt-5 ">
-          <p className='title1 mt-5 ms-5 text-center text-lg-start'>Fitness Club</p>
-          <p className='title2 mt-3 ms-5 text-center text-lg-start'>Sweat, Smile <br />and Repeat</p>
-          <p className='title3 mt-4 ms-5 text-center text-lg-start'>Check out the most effective exercise personnel</p>
-          <button className='btn p-2 mt-5 ms-5 mb-4'>Explore Exercises</button>
+      <div className='row  '>
+        <div className="hero col-lg-6 col-12 mt-5 mt-2">
+          <p className={width? 'title1 mt-5 text-center  ':'title1 mt-5 ms-5 text-center text-lg-start'}>Fitness Club</p>
+          <p className={width ?'title2 mt-4   text-center ':'title2 mt-3 ms-5 text-center text-lg-start'}>Sweat, Smile <br />and Repeat</p>
+          <p className={width?'title3 mt-4 text-center ms-1':'title3 mt-4 ms-5 text-center text-lg-start'}>Check out the most effective exercise personnel</p>
+          <button className={width ? 'btn p-2 d-flex mx-auto mt-5 mb-4':'btn p-2 mt-5 ms-5 mb-4'}>Explore Exercises</button>
 
           {/* <p className='title4 text-center text-lg-start '>Exercice</p> */}
         </div>
-        <div className="col-lg-6 col-md-6 col-12">
+        <div className={width?"col-lg-5 col-md-6 col-12 d-flex ms-auto mb-2":"col-lg-5 col-md-6 col-12"}>
           <img src={banner} alt="banner" className='banner_img img col-lg-6 col-md-12 col-12  ' />
         </div>
  
--- src/components/SearchExercices.jsx
+++ src/components/SearchExercices.jsx
@@ -9,6 +9,9 @@ const SearchExercices = () => {
   useEffect(() => {
     if (window.innerWidth < 992) {
       setWidth(true);
+      console.log(window.location.href);
+      // window.location.reload();
+      // window.location.href = window.location.href;
     }
   }, []);
 
@@ -21,7 +24,7 @@ const SearchExercices = () => {
         <div className="col-12">
           <p className={width ? 'title5resize text-center mt-5': 'title5 text-center mt-5   ' }>Awesome Exercices You <br /> Should Know</p>
           <div className="input col-12  d-flex justify-content-center">
-            <input type="text" placeholder='Search Exercices' className='input1 col-lg-8 col-12 p-2' />
+            <input type="text" placeholder='Search Exercices' className='input1 col-lg-8 col-5 col-12 p-2' />
             
               <button className='btn col-lg-1 ms-4 p-2 '>Search</button>
  
